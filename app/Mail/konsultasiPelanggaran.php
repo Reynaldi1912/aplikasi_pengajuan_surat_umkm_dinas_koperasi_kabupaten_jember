2d5480475cf01ac5ae87338567e5d4f2b98e734d
rizkinahayyuni <rizkinahayyuni@gmail.com>
2021-11-23 11:01:31 +0700

    fitur 4

---
+++ app/Mail/konsultasiPelanggaran.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Mail;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Mail\Mailable;
+use Illuminate\Queue\SerializesModels;
+
+class konsultasiPelanggaran extends Mailable
+{
+    use Queueable, SerializesModels;
+
+    /**
+     * Create a new message instance.
+     *
+     * @return void
+     */
+    public function __construct($day,$date,$day_next,$date_next,$konsultasi)
+    {
+        $this->day = $day;
+        $this->date = $date;
+        $this->day_next = $day_next;
+        $this->date_next = $date_next;
+        $this->konsultasi = $konsultasi;
+    }
+
+    /**
+     * Build the message.
+     *
+     * @return $this
+     */
+    public function build()
+    {
+        return $this->view('notifikasi.konsultasi-pelanggaran');
+    }
+}
