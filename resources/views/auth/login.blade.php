65131ef99dd34314cf5f594f006d3cf55ebc4e2f
Genadi Dharma <genadislawa@gmail.com>
2021-12-29 23:58:42 +0800

    add host name and host address

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -19,6 +19,7 @@
 <div class="login-box">
     <div class="login-logo">
         <a href="{{ url('/home') }}"><b>DISKOPUM JEMBER</b></a>
+        <p style="font-size: 10pt;">From: {{getHostName()}} @ {{request()->getHttpHost()}}</p>
     </div>
     <!-- /.login-logo -->
 
